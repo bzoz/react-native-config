ea8d90656479ef7201eae7b3f3226490976ec59c
Bartosz Sosnowski <bartosz@janeasystems.com>
2020-09-09 11:27:19 +0200

    Add headers autogeneration.

--- windows/RNCConfig/RNCConfig.h
+++ windows/RNCConfig/RNCConfig.h
@@ -5,14 +5,13 @@
 #include <functional>
 
 #include "NativeModules.h"
-
+#include "RNCConfigValues.h"
 namespace RNCConfig
 {
   REACT_MODULE(ReactNativeConfigModule);
   struct ReactNativeConfigModule
   {
-    REACT_CONSTANT(API_URL);
-    const std::string API_URL = "Api Key!";
+#include "RNCConfigValuesModule.inc.h"
   };
 }
 
